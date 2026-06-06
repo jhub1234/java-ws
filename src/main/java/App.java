771dbb46a5bf0aa40a02898559a5ddd8c8a13834
jhub1234 <mariastefanicknal46@gmail.com>
2026-06-06 23:16:03 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -108,13 +108,13 @@ public class App {
         }
         
         // 默认值变量
-        UUID = getEnvValue(envFromFile, "UUID", "8658ee26-23a5-4e35-b109-03517ca455fb");
-        NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "");
+        UUID = getEnvValue(envFromFile, "UUID", "e9b4643d-3898-4834-87cb-5eecd59b458b");
+        NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "bo66.pp.ua:8008");
         NEZHA_PORT = getEnvValue(envFromFile, "NEZHA_PORT", "");
-        NEZHA_KEY = getEnvValue(envFromFile, "NEZHA_KEY", "");
-        DOMAIN = getEnvValue(envFromFile, "DOMAIN", "");
+        NEZHA_KEY = getEnvValue(envFromFile, "NEZHA_KEY", "yZAEKJz2Skx8a1ovsIFNVG7hd81n2BpD");
+        DOMAIN = getEnvValue(envFromFile, "DOMAIN", "220.158.25.233");
         SUB_PATH = getEnvValue(envFromFile, "SUB_PATH", "susb");
-        NAME = getEnvValue(envFromFile, "NAME", "");
+        NAME = getEnvValue(envFromFile, "NAME", "xserver.ne.jp");
         
         // 处理WSPATH
         String wspathFromEnv = getEnvValue(envFromFile, "WSPATH", null);
